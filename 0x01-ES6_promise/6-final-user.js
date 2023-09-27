83957e13078f6d0b95217dd63b99ece1ba493e1f
Kaych3e <nkaduba@gmail.com>
2023-09-27 19:25:34 +0000

    ES6 Promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfilesignup(
+export default function handleProfilesignup(
   firstName,
   lastName,
   fileName,
