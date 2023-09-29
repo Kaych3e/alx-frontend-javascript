150310de0bb0d745407d2e5a4b649f489fa1fc8c
Kaych3e <nkaduba@gmail.com>
2023-09-29 09:31:16 +0000

    6 update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,31 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const res = [];
-  try {
-    const user = signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: err.toString(),
-    });
-  }
-
-  try {
-    const upload = uploadPhoto(fileName);
-    res.push({ status: 'fulfilled', value: upload });
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: err.toString(),
-    });
-  }
-
-  return res;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
+  });
 }
