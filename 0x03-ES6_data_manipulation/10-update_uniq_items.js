8a0619a9e3421f112e0e0e115ae51a3ee4b1ec3e
Kaych3e <nkaduba@gmail.com>
2023-10-03 19:08:10 +0000

    data man

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@ export default function updateUniqueItems(map) {
   if (map instanceof Map) {
     map.forEach((value, key) => {
       if (value === 1) {
-        map.set(key,100);
+        map.set(key, 100);
       }
     });
     return map;
