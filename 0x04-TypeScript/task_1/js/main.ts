1a9b6c4420a58277cca9cc862ed4e2fdbebf8744
Kaych3e <nkaduba@gmail.com>
2023-10-04 21:33:46 +0000

    tS

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,3 +5,40 @@ interface Teacher {
   yearsOfExperience?: number;
   location: string;
   contract: boolean;
+
+// create interface Director
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+//Write function printTeacher
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassinterface;
+}
+
+interface StudentClassInt {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInt {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+};
+
+export { printTeacher, Studentclass };
